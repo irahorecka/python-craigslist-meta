f9d445cefe8803257fd06764d5ccaa2e45c9c434
Ira Horecka <ira89@icloud.com>
2020-12-24 10:53:45 -0800

    add metadata information for better packaging

--- setup.py
+++ setup.py
@@ -1,9 +1,30 @@
+import craigslist_meta
 from setuptools import setup, find_packages
 
-setup(
-    name="python-craigslist-meta",
-    version="0.1.1",
-    author="Ira Horecka",
-    author_email="ira89@icloud.com",
-    packages=find_packages()
-)
+
+_classifiers = [
+    "Programming Language :: Python :: 3.5",
+    "Programming Language :: Python :: 3.6",
+    "Programming Language :: Python :: 3.7",
+    "Programming Language :: Python :: 3.8",
+    "Programming Language :: Python :: 3.9",
+    "Intended Audience :: Developers",
+    "License :: OSI Approved :: MIT License",
+    "Topic :: Software Development :: Libraries",
+    "Topic :: Utilities",
+]
+
+if __name__ == "__main__":
+    setup(
+        name="python-craigslist-meta",
+        version=craigslist_meta.__version__,
+        author="Ira Horecka",
+        author_email="ira89@icloud.com",
+        url="https://github.com/irahorecka/python-craigslist-meta",
+        py_modules=["craigslist_meta"],
+        description="A simple API to traverse Craigslist endpoints",
+        long_description=open("README.rst").read(),
+        license="MIT",
+        classifiers=_classifiers,
+        packages=find_packages(),
+    )
