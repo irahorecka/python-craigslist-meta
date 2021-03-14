e1a2e7b2453d37b656fa8559f941be323e286c15
Ira Horecka <ira89@icloud.com>
2021-03-13 21:03:10 -0800

    remove extraneous files from ./tests/

--- tests/boop.py
+++
@@ -1,7 +0,0 @@
-from craigslist_meta import Site, Country
-
-print(Site("eugene").url)
-# print(Country('japan').url)
-print(next(iter(Site("sfbay"))).url)
-
-print(all(item == 200 for item in [200, 200, 2]))
